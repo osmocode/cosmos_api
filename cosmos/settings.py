692450c9531a827d8b598644ccf27e1327e4731e
osmocode <osmocode@gmail.com>
2022-01-10 01:08:13 +0100

    Dockerized

--- cosmos/settings.py
+++ cosmos/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'apps.cosmos_core',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'cosmos.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE':   'django.db.backends.postgresql',
+        'NAME':     'cosmos_django',
+        'USER':     'cosmos_user',
+        'PASSWORD': 'cosmos_password',
+        'HOST':     'cosmos_postgres',
+        'POST':     '5432',
     }
 }
 
